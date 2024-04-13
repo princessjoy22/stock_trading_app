750aaea5e4e81c822cb5dc6464e506ba087b0127
princessjoy22 <princessjoyibarretapopanes@gmail.com>
2024-04-13 09:31:07 +0800

    Once applied, will add stock model

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,6 +3,7 @@ class StocksController < ApplicationController
     def index
         client = IEX::Api::Client.new
         @most_active_stocks = client.stock_market_list(:mostactive)
+        
     end
     
 end
